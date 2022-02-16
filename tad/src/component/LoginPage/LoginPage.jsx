2dfb11c387238f01b4698e994eacffed329c9cb8
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-02-16 23:24:08 +0700

    login page 50%

--- tad/src/component/LoginPage/LoginPage.jsx
+++ tad/src/component/LoginPage/LoginPage.jsx
@@ -6,7 +6,36 @@ import "./assets/loginpage.css"
 export default function LoginPage() {
   return (
     <div className='LoginPage'>
-      alo
+      <div className="login-page-header">
+           <div className="login-page-header__nav-bar">
+           <div className="login-page-header__nav-bar-logo">
+             logo here
+           </div>
+           <div className="login-page-header__nav-bar-list">
+              <div className="login-page-header__nav-bar-list-item">Premium</div>
+              <div className="login-page-header__nav-bar-list-item">Hổ Trợ</div>
+              <div className="login-page-header__nav-bar-list-item">Tải xuống</div>
+              <div className="login-page-header__nav-bar-list-item">Đăng kí</div>
+              <div className="login-page-header__nav-bar-list-item">Đăng nhập</div>
+           </div>
+           </div>
+      </div>
+
+      <div className="login-page-body">
+      <div className="login-page-body__description">
+        Nghe đa chiều, sống đa sắc
+      </div>
+      <div className="login-page-body__sub-description">
+      Hàng triệu bài hát và podcast. Không cần thẻ tín dụng.
+      </div>
+      <div className="login-page-body__btn">
+        Tải spotify miễn phí
+      </div>
+      </div>
+
+     <div className="login-page-footer">
+
+     </div>
     </div>
   )
 }
