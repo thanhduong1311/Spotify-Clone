e3e06c22e9ba04d3d8a443d7a081d7b73def1d94
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-02-17 22:09:04 +0700

    99% login page

--- tad/src/component/LoginPage/LoginPage.jsx
+++ tad/src/component/LoginPage/LoginPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button } from '@mui/material'
 import "./assets/loginpage.css"
-
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function LoginPage() {
   return (
@@ -29,12 +31,57 @@ export default function LoginPage() {
       Hàng triệu bài hát và podcast. Không cần thẻ tín dụng.
       </div>
       <div className="login-page-body__btn">
-        Tải spotify miễn phí
+        Tải T & D miễn phí
       </div>
       </div>
 
      <div className="login-page-footer">
+         <div className="login-page-footer__grid">
+           <div className="login-page-footer__grid col col-2">LOGO</div>
+           <div className="login-page-footer__grid col col-2">CÔNG TY
+           <div className="login-page-footer__grid-item">
+             Giới Thiệu
+           </div>
+           </div>
+           <div className="login-page-footer__grid col col-2">CỘNG ĐỒNG
+           <div className="login-page-footer__grid-item">
+             Dành cho nghệ sĩ
+           </div>
+           <div className="login-page-footer__grid-item">
+             Nhà phát triển
+           </div>
+           <div className="login-page-footer__grid-item">
+             Quảng cáo
+           </div>
+           <div className="login-page-footer__grid-item">
+             Đóng góp
+           </div>
+           </div>
+           <div className="login-page-footer__grid col col-2">LIÊN KẾT HỮU ÍCH
+           <div className="login-page-footer__grid-item">
+             Hổ trợ
+           </div>       <div className="login-page-footer__grid-item">
+             Trình phát Web
+           </div>       
+           </div>
+           <div className="login-page-footer__grid col col-2"></div>
+           <div className="login-page-footer__grid col col-2">
+           <div className="login-page-footer__grid-social">
 
+             <div className="login-page-footer__grid-social-item">
+               <a href=""><InstagramIcon style={{ fontSize:28}}></InstagramIcon></a></div>
+             <div className="login-page-footer__grid-social-item">
+               <a href=""><FacebookIcon style={{ fontSize:28}}></FacebookIcon></a></div>
+             <div className="login-page-footer__grid-social-item">
+               <a href=""><GitHubIcon style={{ fontSize:28}}></GitHubIcon></a></div>
+           </div>
+           </div>
+
+
+         </div>
+         <div className="login-page-footer__copy-right">
+           Power By T&d
+         </div>
      </div>
     </div>
   )
