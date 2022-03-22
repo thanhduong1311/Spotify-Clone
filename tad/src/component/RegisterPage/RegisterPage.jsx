ad29f49af5596bdd4c139e829298755509913286
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-03-22 18:09:31 +0700

    page

--- tad/src/component/RegisterPage/RegisterPage.jsx
+++ tad/src/component/RegisterPage/RegisterPage.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import "./assets/registerpage.css"
 import FacebookIcon from '@mui/icons-material/Facebook';
 import AppleIcon from '@mui/icons-material/Apple';
-import { Avatar , TextField , Button, FormControlLabel } from '@mui/material';
+import { Avatar , TextField , FormControlLabel } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
+import { Button } from '@mui/material';
 
 const logogg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj5AvPJ_t72_pkXu7lGnKo0Qva4z9UHpTVYQ&usqp=CAU" 
 const logoph = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxZNy6zYaV13uWKvLA7d3olQHroj5ktxuUCw&usqp=CAU"
@@ -25,30 +26,22 @@ export default function RegisterPage() {
                 <FacebookIcon></FacebookIcon>
                  <span> Đăng ký với faceBook </span>
             </button>
-            <button className="RegisterPage-body__btn RegisterPage-body__btn--apple">
-                <AppleIcon></AppleIcon>
-                 <span> Đăng ký với apple </span>
-            </button>
+         
             <button className="RegisterPage-body__btn RegisterPage-body__btn--google">
             <Avatar sx={{width: 20,height : 20}} src={logogg} />
                  <span> Đăng ký với google </span>
             </button>
-           
-            <button className="RegisterPage-body__btn RegisterPage-body__btn--phone-number">
-                 <span> Đăng ký với số điện thoại </span>
-            </button>
+        
             <div className="RegisterPage-body__message">
                  <span className='line'></span> 
                  Hoặc 
                  <span className='line'></span>
                 </div>
-                <button className="RegisterPage-body__btn RegisterPage-body__btn--phone-number">
-                 <span> Đăng ký T&D account</span>
-            </button>
-                 
+
+          
          
-            <button className="RegisterPage-body__btn RegisterPage-body__btn--not-account">
-                 <span> Đăng kí T & D ngay </span>
+            <button className="RegisterPage-body__btn RegisterPage-body__btn-register">
+                 <span> Đăng kí</span>
             </button>
         </div>
      </div>
